07532792241a3ad1656e65a8a6a106f233c72e48
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2024-04-15 23:58:50 +0300

    home page

--- Films/Films/Controllers/HomeController.cs
+++ Films/Films/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Films.Models;
+﻿using AutoMapper;
+using Films.Data;
+using Films.Models;
+using Films.Models.DTO;
+using Films.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +11,46 @@ namespace Films.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _db;
+        public IMapper _mapper { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            AppDbContext db,
+            IMapper mapper
+            )
         {
             _logger = logger;
+            _db = db;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var filmList = _db.Films.Where(u => u.DeleteTime == null);
+            var filmDtoList = new List<FilmDTO>();
+
+            foreach ( var film in filmList )
+            {
+                FilmDTO filmDTO = _mapper.Map<FilmDTO>( film );
+
+                var FilmCategory = _db.FilmCategories.Where(u =>
+                                        u.DeleteTime == null
+                                        && u.FilmId == film.Id).Select(u => u.CategoryId);
+
+                filmDTO.CategoryList = _db.Categories.Where(u => FilmCategory.Contains(u.Id)).ToList();
+
+                filmDtoList.Add(filmDTO);
+            }
+
+            HomeVM homeVM = new HomeVM()
+            {
+                FilmDTO = filmDtoList,
+                Category = _db.Categories.Where(u => u.DeleteTime == null)
+            };
+
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ Films/Films/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using Films.Models.DTO;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Films.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<FilmDTO> FilmDTO { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+
+    }
+}
